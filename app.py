c56396116c06807f06c26e4f53d05e48819fa383
Su Zhou <zhousu@amazon.com>
2022-10-27 23:59:08 +0000

    update stack name

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ import os
 from aws_cdk import core
 from batch_job_cdk.stack import BatchJobStack
 
+
 def get_mandatory_env(name):
-    '''
+    """
     Reads the env variable, raises an exception if missing.
-    '''
+    """
     if name not in os.environ:
         raise Exception("Missing os enviroment variable '%s'" % name)
     return os.environ.get(name)
+
 
 cdk_default_account = get_mandatory_env("CDK_DEPLOY_ACCOUNT")
 cdk_default_region = get_mandatory_env("CDK_DEPLOY_REGION")
 
 app = core.App()
-env = core.Environment(account=cdk_default_account,
-                       region=cdk_default_region)
+env = core.Environment(account=cdk_default_account, region=cdk_default_region)
 
-BatchJobStack(app, 'aws-batch-job-stack', env=env)
+BatchJobStack(app, "automm-cv-bench-batch-job-stack", env=env)
 app.synth()
